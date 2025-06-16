deee92ca964bf2c122d6a3c7106a90c753774166
vlopes <vlopes@Viniciuss-MacBook-Pro.local>
2025-06-16 19:36:53 -0400

    Initial Setup

---
+++ chatgpt_version.py
@@ -0,0 +1,13 @@
+
+
+import requests
+from bs4 import BeautifulSoup
+import csv
+import re
+import os
+import time
+
+BASE_URL = "https://lists.openwall.net/linux-cve-announce"
+OUTPUT_FILE = "cve_commits.csv"
+START_YEAR_MONTH = "2024/02"
+END_YEAR_MONTH = "2025/06"
