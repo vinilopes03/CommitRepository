56faf1b6de313c604f0df30717d833d2adafdc44
vlopes <vlopes@Viniciuss-MacBook-Pro.local>
2025-06-17 17:35:58 -0400

    Add ServletMain.java

--- CWE835_Infinite_Loop/ServletMain.java
+++ CWE835_Infinite_Loop/ServletMain.java
@@ -1,12 +1,13 @@
-// Removed unnecessary imports
+// Added ServletMain class
 package testcases.CWE835_Infinite_Loop;
 
 import javax.servlet.http.*;
 
 public class ServletMain extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L; /* needed since Servlets are serializable */
 	
+	
+
 	public void doPost(HttpServletRequest request, HttpServletResponse response) {
-		doGet(request, response);
-	}
+	...
