88df361b1518559b1d17275bd02b273583aecddb
vlopes <vlopes@Viniciuss-MacBook-Pro.local>
2025-06-17 18:43:32 -0400

    ** Initial setup

---
+++ TEST2/ServletMain.java
@@ -0,0 +1,16 @@
+package testcases.CWE835_Infinite_Loop;
+
+import javax.servlet.http.*;
+
+public class ServletMain extends HttpServlet {
+
+	private static final long serialVersionUID = 1L; /* needed since Servlets are serializable */
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) {
+		doGet(request, response);
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+		doGetCWE1(request, response);
+	}
+}
