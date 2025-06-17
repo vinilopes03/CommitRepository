41e08a1c13e5f9827455d9e6993e7b64bc8d32f0
vlopes <vlopes@Viniciuss-MacBook-Pro.local>
2025-06-17 17:27:28 -0400

    Initial project setup

--- CWE835_Infinite_Loop/antbuild/testcases/CWE835_Infinite_Loop/Main.java
+++ CWE835_Infinite_Loop/antbuild/testcases/CWE835_Infinite_Loop/Main.java
@@ -1,4 +1,1 @@
-	System.err.println("To use this main, you can either run the program with no " +
-				"command line arguments or with -h or --help for usage information.");
-
-These are the first few steps in developing the Java project. Let me know if you want to see more steps or if you have any specific requirements.
+// No code changes, initial setup
