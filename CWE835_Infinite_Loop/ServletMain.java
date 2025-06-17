53211736a534d9a27a957a773899db4d92692910
vlopes <vlopes@Viniciuss-MacBook-Pro.local>
2025-06-17 17:32:27 -0400

    Refactor ServletMain class

--- CWE835_Infinite_Loop/ServletMain.java
+++ CWE835_Infinite_Loop/ServletMain.java
@@ -1,3 +1,12 @@
-public void doGet(HttpServletRequest request, HttpServletResponse response) {
-    // Implement doGet functionality here
-}
+// Removed unnecessary imports
+package testcases.CWE835_Infinite_Loop;
+
+import javax.servlet.http.*;
+
+public class ServletMain extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) {
+		doGet(request, response);
+	}
