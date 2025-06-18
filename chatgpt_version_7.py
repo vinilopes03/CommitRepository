095719e670421e113d901eeca2dc1e0f3a600912
vlopes <vlopes@mac.dhcp.nd.edu>
2025-06-18 13:14:58 -0400

    Initial Setup

---
+++ chatgpt_version_7.py
@@ -0,0 +1,15 @@
+
+
+# Commit 1: Initial Setup
+
+import requests
+from bs4 import BeautifulSoup
+import csv
+import re
+import os
+import time
+
+BASE_URL = "https://lists.openwall.net/linux-cve-announce"
+OUTPUT_FILE = "cve_commits.csv"
+START_YEAR_MONTH = "2024/02"
+END_YEAR_MONTH = "2025/06"
