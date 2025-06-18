52b7124057207e8e3afbf604b8ef16cf21774594
vlopes <vlopes@mac.dhcp.nd.edu>
2025-06-18 14:42:47 -0400

    Add imports and class declaration

--- GENERATED-FINAL/ServletMain.java
+++ GENERATED-FINAL/ServletMain.java
@@ -4,3 +4,13 @@ import javax.servlet.http.*;
 
 public class ServletMain extends HttpServlet {
 
+package testcases.CWE835_Infinite_Loop;
+
+import javax.servlet.http.*;
+
+public class ServletMain extends HttpServlet {
+
+	private static final long serialVersionUID = 1L; /* needed since Servlets are serializable */
+}
+
+}
