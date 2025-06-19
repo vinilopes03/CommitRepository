b7d5ce8a17a3a2778070c96961bffc18ef25a1e4
vlopes <vlopes@mac.dhcp.nd.edu>
2025-06-19 14:50:21 -0400

    TEST-4o/Main.java: Set up package and class structure

---
+++ TEST-4o/Main.java
@@ -0,0 +1,10 @@
+package testcases.CWE835_Infinite_Loop;
+
+import testcasesupport.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Entry point for the program
+    }
+}
