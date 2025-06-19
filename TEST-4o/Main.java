e79653d536f48a64160a9e39cc9ec2c86d3cb7ad
vlopes <vlopes@mac.dhcp.nd.edu>
2025-06-19 14:50:22 -0400

    TEST-4o/Main.java: Implement command-line argument handling and help information

--- TEST-4o/Main.java
+++ TEST-4o/Main.java
@@ -5,6 +5,17 @@ import testcasesupport.*;
 public class Main {
 
     public static void main(String[] args) {
-        // Entry point for the program
+
+        if (args.length != 0) {
+
+            if (args[0].equals("-h") || args[0].equals("--help")) {
+
+                System.err.println("To use this main, you can either run the program with no " +
+                "command line arguments to run all test cases or you can specify one or more classes to test");
+                System.err.println("For example:");
+                System.err.println("java testcasesupport.Main testcases.CWE690_Unchecked_Return_Value_to_NULL_Pointer_Dereference.custom_function.CWE690_Unchecked_Return_Value_to_NULL_Pointer_Dereference__custom_function_01 testcases.CWE481_Assigning_instead_of_Comparing.bool.CWE481_Assigning_instead_of_Comparing__bool_01");
+                System.exit(1);
+            }
+        }
     }
 }
