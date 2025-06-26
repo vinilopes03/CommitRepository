d5e3893d48c56812cd75218ab27f2595d9de046e
vlopes <vlopes@mac.dhcp.nd.edu>
2025-06-26 18:06:00 -0400

    initial setup

--- CWE835-InfiniteLoopTestProject5/src/test/java/CWE835_Infinite_Loop__while_true_01Test.java
+++ CWE835-InfiniteLoopTestProject5/src/test/java/CWE835_Infinite_Loop__while_true_01Test.java
@@ -7,21 +7,18 @@ import java.util.concurrent.TimeUnit;
 public class CWE835_Infinite_Loop__while_true_01Test {
 
     @Test
-    @Timeout(value = 2, unit = TimeUnit.SECONDS)
-    public void testInfiniteLoopVulnerability() {
+    @Timeout(value = 1, unit = TimeUnit.SECONDS)
+    public void testBadMethodForInfiniteLoop() {
         CWE835_Infinite_Loop__while_true_01 instance = new CWE835_Infinite_Loop__while_true_01();
-        boolean isVulnerable = false;
         
         try {
             instance.bad();
+            // If the method returns, it means there was no infinite loop
+            // Therefore, the test should fail because we expect an infinite loop
+            assertTrue(false, "The method did not result in an infinite loop as expected.");
         } catch (Exception e) {
-            // If an exception occurs, it means the loop was broken due to an error, which is not expected.
-            isVulnerable = false;
+            // If an exception occurs, it should not be considered as a successful test
+            assertTrue(false, "An unexpected exception occurred: " + e.getMessage());
         }
-        
-        // If the method does not complete within the timeout, it is vulnerable.
-        isVulnerable = true;
-        
-        assertTrue(isVulnerable, "The method is not vulnerable to an infinite loop.");
     }
 }
