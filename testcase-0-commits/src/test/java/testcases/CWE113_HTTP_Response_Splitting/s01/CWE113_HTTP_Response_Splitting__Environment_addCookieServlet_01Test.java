285eabf8051e88ff58b3a6e0c58bf665cf4ec88a
vlopes <vlopes@Viniciuss-MacBook-Pro.local>
2025-07-23 23:38:29 -0400

    initial setup

--- testcase-0-commits/src/test/java/testcases/CWE113_HTTP_Response_Splitting/s01/CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01Test.java
+++ testcase-0-commits/src/test/java/testcases/CWE113_HTTP_Response_Splitting/s01/CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01Test.java
@@ -18,20 +18,42 @@ public class CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01Test
         HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
         HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
 
+        // Create an instance of the class to be tested
+        CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01 servlet =
+                new CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01();
+
         // Set up the environment variable
-        String maliciousInput = "en-US\r\nSet-Cookie: sessionId=malicious";
-        System.setProperty("ADD", maliciousInput);
+        String maliciousData = "test\r\nSet-Cookie: sessionId=malicious";
+        System.setProperty("ADD", maliciousData);
+
+        // Call the bad method
+        servlet.bad(request, response);
+
+        // Verify that a cookie with the malicious data was added
+        Mockito.verify(response).addCookie(Mockito.argThat(cookie -> 
+            cookie.getValue().equals(maliciousData)
+        ));
+
+        // Clean up the environment variable
+        System.clearProperty("ADD");
+    }
+
+    @Test
+    public void testGoodG2B() throws Throwable {
+        // Mock the HttpServletRequest and HttpServletResponse
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
 
         // Create an instance of the class to be tested
         CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01 servlet =
                 new CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01();
 
-        // Call the bad method
-        servlet.bad(request, response);
+        // Call the goodG2B method
+        servlet.goodG2B(request, response);
 
-        // Verify that a cookie with the malicious input was added
+        // Verify that a cookie with the hardcoded safe data was added
         Mockito.verify(response).addCookie(Mockito.argThat(cookie -> 
-            cookie.getValue().equals(maliciousInput)
+            cookie.getValue().equals("foo")
         ));
     }
 
@@ -41,20 +63,23 @@ public class CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01Test
         HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
         HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
 
-        // Set up the environment variable
-        String maliciousInput = "en-US\r\nSet-Cookie: sessionId=malicious";
-        System.setProperty("ADD", maliciousInput);
-
         // Create an instance of the class to be tested
         CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01 servlet =
                 new CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01();
 
+        // Set up the environment variable
+        String maliciousData = "test\r\nSet-Cookie: sessionId=malicious";
+        System.setProperty("ADD", maliciousData);
+
         // Call the goodB2G method
         servlet.goodB2G(request, response);
 
-        // Verify that a cookie with the malicious input was not added directly
+        // Verify that a cookie with the encoded data was added
         Mockito.verify(response).addCookie(Mockito.argThat(cookie -> 
-            !cookie.getValue().equals(maliciousInput)
+            !cookie.getValue().equals(maliciousData) && cookie.getValue().contains("test%0D%0ASet-Cookie%3A+sessionId%3Dmalicious")
         ));
+
+        // Clean up the environment variable
+        System.clearProperty("ADD");
     }
 }
