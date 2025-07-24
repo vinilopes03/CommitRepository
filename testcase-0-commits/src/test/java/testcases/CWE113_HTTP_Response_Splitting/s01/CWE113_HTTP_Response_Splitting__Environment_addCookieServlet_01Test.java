edae11685e9ba733f574caf5b69b6ef8e9b8f5cc
vlopes <vlopes@Viniciuss-MacBook-Pro.local>
2025-07-23 23:22:29 -0400

    initial setup

--- testcase-0-commits/src/test/java/testcases/CWE113_HTTP_Response_Splitting/s01/CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01Test.java
+++ testcase-0-commits/src/test/java/testcases/CWE113_HTTP_Response_Splitting/s01/CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01Test.java
@@ -13,50 +13,48 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 public class CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01Test {
 
     @Test
-    public void testBadMethodVulnerability() throws Throwable {
+    public void testBad() throws Throwable {
         // Mock the HttpServletRequest and HttpServletResponse
         HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
         HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
 
         // Set up the environment variable
-        String maliciousInput = "value%0D%0ASet-Cookie:%20sessionId=malicious";
+        String maliciousInput = "en-US\r\nSet-Cookie: sessionId=malicious";
         System.setProperty("ADD", maliciousInput);
 
-        // Create an instance of the class to test
+        // Create an instance of the class to be tested
         CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01 servlet =
                 new CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01();
 
         // Call the bad method
         servlet.bad(request, response);
 
-        // Capture the added cookie
-        Mockito.verify(response).addCookie(Mockito.argThat(cookie -> {
-            // Check if the cookie value contains CRLF, indicating a vulnerability
-            return cookie.getValue().contains("\r\n");
-        }));
+        // Verify that a cookie with the malicious input was added
+        Mockito.verify(response).addCookie(Mockito.argThat(cookie -> 
+            cookie.getValue().equals(maliciousInput)
+        ));
     }
 
     @Test
-    public void testGoodB2GMethodNoVulnerability() throws Throwable {
+    public void testGoodB2G() throws Throwable {
         // Mock the HttpServletRequest and HttpServletResponse
         HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
         HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
 
         // Set up the environment variable
-        String maliciousInput = "value%0D%0ASet-Cookie:%20sessionId=malicious";
+        String maliciousInput = "en-US\r\nSet-Cookie: sessionId=malicious";
         System.setProperty("ADD", maliciousInput);
 
-        // Create an instance of the class to test
+        // Create an instance of the class to be tested
         CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01 servlet =
                 new CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01();
 
         // Call the goodB2G method
         servlet.goodB2G(request, response);
 
-        // Capture the added cookie
-        Mockito.verify(response).addCookie(Mockito.argThat(cookie -> {
-            // Check if the cookie value does not contain CRLF, indicating no vulnerability
-            return !cookie.getValue().contains("\r\n");
-        }));
+        // Verify that a cookie with the malicious input was not added directly
+        Mockito.verify(response).addCookie(Mockito.argThat(cookie -> 
+            !cookie.getValue().equals(maliciousInput)
+        ));
     }
 }
