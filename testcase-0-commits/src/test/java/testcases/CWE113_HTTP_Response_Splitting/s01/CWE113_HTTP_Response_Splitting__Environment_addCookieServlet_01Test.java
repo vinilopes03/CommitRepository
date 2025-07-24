563e96efcba9fa1a886e60a07eba7adeb369a6d5
vlopes <vlopes@Viniciuss-MacBook-Pro.local>
2025-07-24 00:07:56 -0400

    initial setup

--- testcase-0-commits/src/test/java/testcases/CWE113_HTTP_Response_Splitting/s01/CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01Test.java
+++ testcase-0-commits/src/test/java/testcases/CWE113_HTTP_Response_Splitting/s01/CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01Test.java
@@ -8,69 +8,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.*;
 
 public class CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01Test {
 
     @Test
-    public void testBad() throws Throwable {
-        // Mock the HttpServletRequest and HttpServletResponse
-        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
-        HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+    public void testBadMethodVulnerability() throws Throwable {
+        // Arrange
+        CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01 servlet = new CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01();
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
 
-        // Set up the environment variable to simulate a malicious input
-        String maliciousInput = "en-US\r\nSet-Cookie: sessionId=malicious";
-        System.setProperty("ADD", maliciousInput);
+        // Mock environment variable
+        String maliciousInput = "value\r\nSet-Cookie: sessionId=malicious";
+        Mockito.mockStatic(System.class);
+        when(System.getenv("ADD")).thenReturn(maliciousInput);
 
-        // Create an instance of the class to be tested
-        CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01 servlet =
-                new CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01();
-
-        // Invoke the bad method
+        // Act
         servlet.bad(request, response);
 
-        // Verify that the response added a cookie with the malicious input
-        Mockito.verify(response).addCookie(Mockito.argThat(cookie -> cookie.getValue().equals(maliciousInput)));
-
-        // Assert that the vulnerability is present
-        assertTrue(isVulnerable(response, maliciousInput));
-    }
-
-    @Test
-    public void testGoodB2G() throws Throwable {
-        // Mock the HttpServletRequest and HttpServletResponse
-        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
-        HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
-
-        // Set up the environment variable to simulate a malicious input
-        String maliciousInput = "en-US\r\nSet-Cookie: sessionId=malicious";
-        System.setProperty("ADD", maliciousInput);
-
-        // Create an instance of the class to be tested
-        CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01 servlet =
-                new CWE113_HTTP_Response_Splitting__Environment_addCookieServlet_01();
-
-        // Invoke the goodB2G method
-        servlet.goodB2G(request, response);
-
-        // Verify that the response added a cookie with the encoded input
-        Mockito.verify(response).addCookie(Mockito.argThat(cookie -> !cookie.getValue().equals(maliciousInput)));
-
-        // Assert that the vulnerability is not present
-        assertFalse(isVulnerable(response, maliciousInput));
-    }
-
-    private boolean isVulnerable(HttpServletResponse response, String input) {
-        // This method checks if the response is vulnerable by inspecting the cookies
-        // added to the response. If a cookie contains the raw input, it indicates a vulnerability.
-        Cookie[] cookies = response.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getValue().equals(input)) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        // Assert
+        // Verify that a cookie was added with the malicious input
+        verify(response).addCookie(argThat(cookie -> cookie.getValue().equals(maliciousInput)));
     }
 }
